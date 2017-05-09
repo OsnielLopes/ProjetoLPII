0ad68b7bdbfe38d7951ecadb71f1ce7bc27a9114
Helder Ardachnikoff <helder.versatti@gmail.com>
2017-05-09 10:20:16 -0300

    Acrescimo no parte de inserção de times pelo usuario.

--- Projeto/src/Tela.java
+++ Projeto/src/Tela.java
@@ -1,13 +1,13 @@
 import javax.swing.*;
 public class Tela {
     public static OpcEntrada opcLeitura(){
-        Object[] opcoes = { "Escrever Times Manualmente", "Importar Times de um arquivo" };
+        Object[] opcoes1 = { "Escrever Times Manualmente", "Importar Times de um arquivo" };
         
-        JPanel panel = new JPanel();
-        panel.add(new JLabel("Escolha uma das opções : "));
+        JPanel panel1 = new JPanel();
+        panel1.add(new JLabel("Escolha uma das opções : "));
         
-        int result = JOptionPane.showOptionDialog(null, panel, "Tabela de Jogos", 
-                     JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes, null);
+        int result = JOptionPane.showOptionDialog(null, panel1, "Tabela de Jogos", 
+                     JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes1, null);
         if (result == JOptionPane.YES_OPTION){
             return OpcEntrada.Console;
         }else{
@@ -23,8 +23,30 @@ public class Tela {
         return quantidade;
     }
     
+    // A tela de interação para pedir: nome - cidade - estado para o usuario
     public static String[] digitaTime(){
-        Object[] opcoes = { "OK", "Cancelar" }; 
-        JPanel
+        Object[] opcoes2 = { "Enviar", "Cancelar" }; 
+        // NOME
+        JPanel panel2 = new JPanel();
+        panel2.add(new JLabel("Nome : "));
+        JTextField textFieldNome = new JTextField(10);
+        panel2.add(textFieldNome);
+        // CIDADE
+        panel2.add(new JLabel("Cidade : "));
+        JTextField textFieldCidade = new JTextField(10);
+        panel2.add(textFieldCidade);
+        // ESTADO
+        panel2.add(new JLabel("Estado : "));
+        JTextField textFieldEstado = new JTextField(10);
+        panel2.add(textFieldEstado);
+        
+        // ARRUMAR O RETORNO DESSE PONTO !!!
+        int result = JOptionPane.showOptionDialog(null, panel2, "Tabela de Jogos", 
+                     JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes2, null);
+        if (result == JOptionPane.YES_OPTION){
+            return null;
+        }else{
+            return null;
+        }
     }
 }
