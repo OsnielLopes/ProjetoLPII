aa902c2e3aeab692dcb88b2625acb1e6573dc4fa
OsnielLopes <niellt@hotmail.com>
2017-05-09 20:47:46 -0300

    atualiza a classe Partida

--- Projeto/src/Partida.java
+++ Projeto/src/Partida.java
@@ -1,4 +1,26 @@
 import java.util.*;
 public class Partida {
+    private Date data;
+    private Time timeA;
+    private Time timeB;
     
+    public Partida(Time timeA, Time timeB, Date data){
+        this.timeA = timeA;
+        this.timeB = timeB;
+        this.data = data;
+
+    }
+    
+    public Date getData(){
+        return this.data;
+    }
+    
+//    public String getLocal(){
+//        return timeA.getLocal();
+//    }
+    
+    public Time[] getAdversários(){
+        Time adversarios[] = {timeA, timeB};
+        return adversarios;
+    }
 }
