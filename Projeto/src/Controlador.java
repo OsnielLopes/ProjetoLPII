cf78f543e1a8abc2c3a7b1a9377beaca1aa18f55
OsnielLopes <niellt@hotmail.com>
2017-05-15 19:52:15 -0300

    atualização

--- Projeto/src/Controlador.java
+++ Projeto/src/Controlador.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Controlador {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         OpcEntrada opc = Tela.opcLeitura();
         if (opc.equals(OpcEntrada.Console)){
             int qtdTimes = Tela.qtdTimes();
