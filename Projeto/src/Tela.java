610a4cde81cb4ffb6628f0232c2784cde14db3c7
OsnielLopes <niellt@hotmail.com>
2017-05-15 17:54:59 -0300

    atualiza a classe tela

--- Projeto/src/Tela.java
+++ Projeto/src/Tela.java
@@ -1,6 +1,9 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.ArrayList;
 import javax.swing.*;
 import org.json.*;
-import java.io.*;
 public class Tela {
     public static OpcEntrada opcLeitura(){
         Object[] opcoes1 = { "Escrever Times Manualmente", "Importar Times de um arquivo" };
@@ -26,29 +29,101 @@ public class Tela {
     }
     
     // A tela de interação para pedir: nome - cidade - estado para o usuario
-    public static String[] digitaTime(){
-        Object[] opcoes2 = { "Enviar", "Cancelar" }; 
-        // NOME
-        JPanel panel2 = new JPanel();
-        panel2.add(new JLabel("Nome : "));
-        JTextField textFieldNome = new JTextField(10);
-        panel2.add(textFieldNome);
-        // CIDADE
-        panel2.add(new JLabel("Cidade : "));
-        JTextField textFieldCidade = new JTextField(10);
-        panel2.add(textFieldCidade);
-        // ESTADO
-        panel2.add(new JLabel("Estado : "));
-        JTextField textFieldEstado = new JTextField(10);
-        panel2.add(textFieldEstado);
+    public static String[] digitaTime() throws FileNotFoundException, IOException{
+        FileReader fr = new FileReader("Estados.json");
+        JSONTokener jsonTokener = new JSONTokener(fr);
+        JSONArray a = new JSONArray(jsonTokener);
+        String[] opcoes = new String[27];
         
-        // ARRUMAR O RETORNO DESSE PONTO !!!
-        int result = JOptionPane.showOptionDialog(null, panel2, "Tabela de Jogos", 
-                     JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoes2, null);
+        for (int i = 0; i < a.length(); i++) {
+            JSONObject jsonobject = a.getJSONObject(i);
+            opcoes[i] = jsonobject.getString("Sigla");
+        }
+        fr.close();
+        
+        fr = new FileReader("Cidades.json");
+        jsonTokener = new JSONTokener(fr);
+        JSONArray cidades = new JSONArray(jsonTokener);
+        ArrayList<String> c = new ArrayList<String>();
+        
+        JFrame janela = new JFrame("Tabela de Jogos");
+        janela.setLocation(500, 300);
+        LayoutManager layout = new GridLayout(5, 3);
+        layout.preferredLayoutSize(janela);
+        janela.setLayout(layout);
+        
+        Container container = janela.getContentPane();
+
+        container.add(new JLabel("Nome : "));
+        JTextField textFieldNome = new JTextField(10);
+        container.add(textFieldNome);
+        
+        JComboBox cursosBox = new JComboBox(opcoes);
+        container.add(cursosBox);
+
+        JComboBox discBox = new JComboBox();
+        container.add(discBox);
+
+        JButton botao = new JButton("Enviar");
+        container.add(botao);
+
+        janela.pack();
+        discBox.setVisible(false);
+
+        cursosBox.addActionListener((ActionEvent e) -> {
+            int indexEstado = cursosBox.getSelectedIndex();
+            if(!(c.isEmpty())){
+                    c.clear();
+                }
+            for (int i = 0; i < cidades.length(); i++) {
+                JSONObject jsonobject = cidades.getJSONObject(i);
+                if (jsonobject.getString("Estado").equals(Integer.toString(indexEstado+1))){
+                    c.add(jsonobject.getString("Nome"));
+                }
+            }
+            String[] stringModel = c.toArray(new String[c.size()]);
+            DefaultComboBoxModel model = new DefaultComboBoxModel(stringModel);
+            discBox.setModel(model);
+            discBox.setVisible(true);
+            janela.pack();
+            
+        });
+        
+        botao.addActionListener((ActionEvent e) -> {
+            janela.dispatchEvent(new WindowEvent(janela, WindowEvent.WINDOW_CLOSING));
+        });
+
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // CIDADE
+//        panel.add(new JLabel("Cidade : "));
+//        String[] options = { "SP","RJ","ES","RS","SC","RO","AC" };
+//        JComboBox comboBox = new JComboBox(options);
+//        panel.add(comboBox);
+//        comboBox.addActionListener((ActionEvent e)-> {
+//            System.out.println("oi");
+//            comboBox.setVisible(true);
+//            panel.revalidate();
+//            panel.repaint();
+//        });
+////        JTextField textFieldCidade = new JTextField(10);
+////        panel2.add(textFieldCidade);
+//        // ESTADO
+//        panel.add(new JLabel("Estado : "));
+//        JTextField textFieldEstado = new JTextField(10);
+//        panel.add(textFieldEstado);
+//        textFieldEstado.setVisible(false);
+//        
+//        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+//        // ARRUMAR O RETORNO DESSE PONTO !!!
+//        panel.setVisible(true);
+        int result = 0;
         if (result == JOptionPane.YES_OPTION){
             return null;
         }else{
             return null;
         }
-    }
+    
+}
 }
